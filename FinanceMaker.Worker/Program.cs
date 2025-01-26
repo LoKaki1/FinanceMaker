4453380041158ebd9d4f60329f696ba6ad6fc767
LoKaki <shahartheking22@gmail.com>
2025-01-26 09:29:23 +0200

    I know codeeeeeeee

---
+++ FinanceMaker.Worker/Program.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using FinanceMaker.Algorithms;
+using FinanceMaker.Algorithms.News.Analyziers;
+using FinanceMaker.Algorithms.News.Analyziers.Interfaces;
+using FinanceMaker.Common;
+using FinanceMaker.Common.Models.Ideas.IdeaInputs;
+using FinanceMaker.Common.Models.Ideas.IdeaOutputs;
+using FinanceMaker.Ideas.Ideas;
+using FinanceMaker.Ideas.Ideas.Abstracts;
+using FinanceMaker.Publisher.Orders.Trader;
+using FinanceMaker.Publisher.Orders.Trader.Interfaces;
+using FinanceMaker.Publisher.Traders;
+using FinanceMaker.Publisher.Traders.Interfaces;
+using FinanceMaker.Pullers;
+using FinanceMaker.Pullers.NewsPullers;
+using FinanceMaker.Pullers.NewsPullers.Interfaces;
+using FinanceMaker.Pullers.PricesPullers;
+using FinanceMaker.Pullers.PricesPullers.Interfaces;
+using FinanceMaker.Pullers.TickerPullers;
+using FinanceMaker.Pullers.TickerPullers.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var app = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHttpClient();
+                // Can't use the extension (the service collection becomes read only after the build)
+                services.AddSingleton<MarketStatus>();
+                services.AddSingleton<FinvizTickersPuller>();
+                services.AddSingleton(sp => new IParamtizedTickersPuller[]
+                {
+                    sp.GetService<FinvizTickersPuller>()
+                });
+                services.AddSingleton(sp => Array.Empty<ITickerPuller>());
+                services.AddSingleton(sp => Array.Empty<IRelatedTickersPuller>());
+                services.AddSingleton<MainTickersPuller>();
+
+                services.AddSingleton<YahooPricesPuller>();
+                services.AddSingleton<YahooInterdayPricesPuller>();
+
+                services.AddSingleton(sp => new IPricesPuller[]
+                {
+                    // sp.GetService<YahooPricesPuller>(),
+                    sp.GetService<YahooInterdayPricesPuller>(),
+
+                });
+                services.AddSingleton<IPricesPuller, MainPricesPuller>();
+                services.AddSingleton<GoogleNewsPuller>();
+                services.AddSingleton<YahooFinanceNewsPuller>();
+                services.AddSingleton(sp => new INewsPuller[]
+                {
+                    sp.GetService<GoogleNewsPuller>(),
+                    sp.GetService<YahooFinanceNewsPuller>(),
+                    sp.GetService<FinvizNewPuller>(),
+
+                });
+
+                services.AddSingleton<KeyLevelsRunner>();
+                services.AddSingleton<EMARunner>();
+                services.AddSingleton<BreakOutDetectionRunner>();
+
+                services.AddSingleton<IEnumerable<IAlgorithmRunner<RangeAlgorithmInput>>>(
+                    sp =>
+                    {
+                        var runner3 = sp.GetService<KeyLevelsRunner>();
+                        var runner1 = sp.GetService<EMARunner>();
+                        var runner2 = sp.GetService<BreakOutDetectionRunner>();
+
+
+                        return [runner1, runner2, runner3];
+                    }
+                );
+
+                services.AddSingleton<RangeAlgorithmsRunner>();
+                services.AddSingleton<INewsPuller, MainNewsPuller>();
+                services.AddSingleton<KeywordsDetectorAnalysed>();
+                services.AddSingleton<INewsAnalyzer[]>(sp => [
+                    sp.GetService<KeywordsDetectorAnalysed>()
+                ]);
+                services.AddSingleton<INewsAnalyzer, NewsAnalyzer>();
+                services.AddSingleton<IdeaBase<TechnicalIdeaInput, EntryExitOutputIdea>, OverNightBreakout>();
+                services.AddSingleton<OverNightBreakout>();
+                services.AddSingleton<IBroker, AlpacaBroker>();
+                services.AddSingleton<ITrader, WorkerTrader>();
+                services.AddHostedService<Worker>();
+
+
+            })
+            .Build();
+app.Run();
---
+++ FinanceMaker.Worker/Worker.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NCrontab;
+using Microsoft.Extensions.Logging;
+using FinanceMaker.Publisher.Traders;
+using FinanceMaker.Publisher.Traders.Interfaces;
+
+public class Worker : BackgroundService
+{
+    private NCrontab.CrontabSchedule m_CrontabSchedule;
+    private readonly ILogger<Worker> m_Logger;
+    private readonly ITrader m_Trader;
+
+    public Worker(ILogger<Worker> logger, ITrader workerTrader)
+    {
+        m_Logger = logger;
+        m_Trader = workerTrader;
+        m_CrontabSchedule = CrontabSchedule.Parse("0 * * * *");
+
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var now = DateTime.Now;
+            DateTime nextExecutionTime = m_CrontabSchedule.GetNextOccurrence(now);
+            await Task.Delay((nextExecutionTime - now).Milliseconds, stoppingToken);
+            await m_Trader.Trade(stoppingToken);
+        }
+    }
+}
