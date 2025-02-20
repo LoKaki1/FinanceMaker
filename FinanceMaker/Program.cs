212aa361bb7bd18999d1c2fb789ee4483fb6046c
LoKaki <shahartheking22@gmail.com>
2025-02-20 09:58:14 +0200

    aaaaa

--- FinanceMaker/Program.cs
+++ FinanceMaker/Program.cs
@@ -1,20 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Net.WebSockets;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
-using FinanceMaker.Common.Extensions;
-using FinanceMaker.Pullers.NewsPullers;
-using FinanceMaker.Pullers.NewsPullers.Interfaces;
-using FinanceMaker.Pullers.PricesPullers;
-using FinanceMaker.Pullers.PricesPullers.Interfaces;
-using FinanceMaker.Pullers.TickerPullers;
-using FinanceMaker.Pullers.TickerPullers.Interfaces;
-using HtmlAgilityPack;
-using Microsoft.Extensions.DependencyInjection;
+
+//using FinanceMaker.Algorithms.QuantConnectAlgorithms;
 using Microsoft.Extensions.Hosting;
 using QuantConnect;
 using QuantConnect.Configuration;
+using QuantConnect.Lean.Engine;
+using QuantConnect.Util;
 
 Console.WriteLine("Hello, World!");
 
@@ -53,6 +44,44 @@ Console.WriteLine("Hello, World!");
 //       |          |
 //       |          V
 //        --- |TradesPublisher| * Publishing to the relevant brokers
-//
+// 
+//   "algorithm-type-name": "ZeroFeeRegressionAlgorithm",
+
+//   // Algorithm language selector - options CSharp, Python
+//   "algorithm-language": "CSharp",
+
+//   //Physical DLL location
+//   "algorithm-location": "QuantConnect.Algorithm.CSharp.dll",
+Config.Set("algorithm-type-name", "AlgorithmBaseForTestingOnly");
+Config.Set("data-folder", "../../../../Lean/Data");
+Config.Set("algorithm-language", "CSharp");
+Config.Set("algorithm-location", "FinanceMaker.dll");
 
 
+//Name thread for the profiler:
+Thread.CurrentThread.Name = "Algorithm Analysis Thread";
+
+Initializer.Start();
+var leanEngineSystemHandlers = Initializer.GetSystemHandlers();
+
+//-> Pull job from QuantConnect job queue, or, pull local build:
+var job = leanEngineSystemHandlers.JobQueue.NextJob(out var assemblyPath);
+
+var leanEngineAlgorithmHandlers = Initializer.GetAlgorithmHandlers();
+
+// Create the algorithm manager and start our engine
+var algorithmManager = new AlgorithmManager(QuantConnect.Globals.LiveMode, job);
+
+leanEngineSystemHandlers.LeanManager.Initialize(leanEngineSystemHandlers, leanEngineAlgorithmHandlers, job, algorithmManager);
+
+OS.Initialize();
+
+var engine = new Engine(leanEngineSystemHandlers, leanEngineAlgorithmHandlers, QuantConnect.Globals.LiveMode);
+engine.Run(job, algorithmManager, assemblyPath, WorkerThread.Instance);
+Console.WriteLine(engine.AlgorithmHandlers.Results);
+
+// Now everything works using the library instead of the cloned code
+// What we need to do now is first oranize all this shit 
+// then understand how we take all the out from this
+// but before don't forget to add the data folder (They need it)
+// 
